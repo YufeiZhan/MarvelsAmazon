61279d119b075654d1c1865c1c469eda581da435
yanzheng.wu@duke.edu <yw609@duke.edu>
2024-03-25 20:01:29 -0400

    implemented product page

--- app/index.py
+++ app/index.py
@@ -12,7 +12,7 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 def index():
     # get all available products for sale:
-    products = Product.get_all(True)
+    products = Product.get_all()
     # find the products current user has bought:
     if current_user.is_authenticated:
         purchases = Purchase.get_all_by_uid_since(
@@ -21,5 +21,4 @@ def index():
         purchases = None
     # render the page by adding information to the index.html file
     return render_template('index.html',
-                           avail_products=products,
-                           purchase_history=purchases)
+                           avail_products=products)
