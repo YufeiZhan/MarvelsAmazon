59309ad32c2275ad2720a7d77706265a427dd2ec
yz858 <selina.zhan@duke.edu>
2024-03-26 05:17:41 -0400

    Integrate Cart flow

--- app/carts.py
+++ app/carts.py
@@ -1,5 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import current_user
+from flask_login import current_user, login_user
+
+from .models.cart import Cart
+from .models.user import User
 
 from flask import Blueprint
 bp = Blueprint('carts', __name__)
@@ -8,4 +11,13 @@ bp = Blueprint('carts', __name__)
 @bp.route('/cart')
 def lookup():
     if current_user.is_authenticated:
-        return render_template('cart.html')
+        # Render the seller inventory page, passing the inventory items for display
+        fake_user =  User(6, '', 'Selina', '', '', balance=10, role_indicator=0)
+        login_user(fake_user)
+
+        # Get all inventory items for the current user/seller
+        cart_items = Cart.get_all_by_uid(current_user.id)
+        return render_template('cart.html', cart_items=cart_items)
+    else:
+        # If the user is not authenticated,  redirect to login page
+        return redirect(url_for('users.login'))
