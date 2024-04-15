76af3f193a9147be22c17fb0ff0febefc6421571
petermin <hm246@duke.edu>
2024-04-15 17:48:24 -0400

    Top up now works.

--- app/models/user.py
+++ app/models/user.py
@@ -66,6 +66,22 @@ class User(UserMixin):
             return None
 
     @staticmethod
+    def topup(id):
+        try:
+            rows = app.db.execute(
+                """
+                UPDATE Users
+                SET balance = balance + 100
+                WHERE uid = :id
+                """,
+                id=id
+            )
+            return rows
+        except Exception as e:
+            print(str(e))
+            return None
+
+    @staticmethod
     def update_user_info(id, email, password, firstname, lastname):
         if User.email_exists(email):
             return None
