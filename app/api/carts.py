4ca49e2fe3d9d4d6950471c03c30a33866b4dac0
Yufei Zhan <selinazhan@Yufeis-MacBook-Pro.local>
2024-04-16 23:02:37 -0400

    Add Cart features to adjust quantity, remove item, show total price (coupon,clean all and submit not yet implemented)

--- app/api/carts.py
+++ app/api/carts.py
@@ -2,29 +2,44 @@ from flask import render_template, redirect, url_for, flash, request, jsonify
 from flask_login import current_user, login_user, login_required
 import math
 
-from ..models.cart import Cart
+from ..models.cart import Cart, CartWithPrice
 from ..models.user import User
 
 from flask import Blueprint
 bp = Blueprint('carts', __name__)
 
 
-@bp.route('/cart/<int:page>')
+# @bp.route('/cart/<int:page>')
 @bp.route('/cart/')
 @login_required
 # flask_login's login_required decorator returns login page automatically if user not logged in
 # this saves long code to check if user is logged in and return
 def lookup(page=1):
     # Get items on the specified page
-    items = Cart.get_page(current_user.id,page)
-    max_page = math.ceil(len(Cart.get_all_by_uid(current_user.id))/Cart.entry_per_page)
+    # items = Cart.get_page(current_user.id,page)
+    # max_page = math.ceil(len(Cart.get_all_by_uid(current_user.id))/Cart.entry_per_page)
 
     # Get all carts items for the current user/seller
-    cart_items = Cart.get_all_by_uid(current_user.id)
-    return render_template('cart.html', cart_items=items, role=User.getRole(current_user.id), page=page, max_page=max_page)
+    cart_items = Cart.get_all_by_uid_with_price(current_user.id)
+    total_price = CartWithPrice.get_total_price(cart_items)
+    # return render_template('cart.html', cart_items=items, role=User.getRole(current_user.id), page=page, max_page=max_page)
+    return render_template('cart.html', cart_items=cart_items, role=User.getRole(current_user.id), total_price=total_price)
+
 
 @bp.route('/cart/remove/<int:inventoryid>')
 @login_required
 def remove_item(inventoryid):
     Cart.remove_item(current_user.id, inventoryid)
     return jsonify({'message': 'Item removed successfully'})
+
+@bp.route('/cart/decrease/<int:uid>/<int:iid>')
+@login_required
+def decrease_item(uid,iid):
+    Cart.decrease_item(uid, iid)
+    return jsonify({'message': 'Item decreased successfully'})
+
+@bp.route('/cart/increase/<int:uid>/<int:iid>')
+@login_required
+def increase_item(uid,iid):
+    Cart.increase_item(uid, iid)
+    return jsonify({'message': 'Item increased successfully'})
--- app/models/cart.py
+++ app/models/cart.py
@@ -11,15 +11,16 @@ class Cart:
         self.status = status
 
     @staticmethod
-    def get_all_by_uid(id):
+    def get_all_by_uid_with_price(id):
         rows = app.db.execute(
             '''
-            SELECT uid, iid, quantity, status
+            SELECT uid, CartItems.iid, quantity, status, unit_price
             FROM CartItems
+            JOIN Inventory USING (iid)
             WHERE uid = :id
             ''',id=id)
         
-        return [Cart(*row) for row in rows] if rows else None
+        return [CartWithPrice(*row) for row in rows] if rows else None
 
     @staticmethod
     def get_page(id,n):
@@ -33,7 +34,7 @@ class Cart:
             LIMIT :n OFFSET :start;
             ''', id=id, n=Cart.entry_per_page, start=offset)
 
-        return [Cart(*row) for row in rows] if rows else None
+        return [CartWithPrice(*row) for row in rows] if rows else None
 
     @staticmethod
     def remove_item(userid,inventoryid):
@@ -41,4 +42,36 @@ class Cart:
             '''
             DELETE FROM CartItems
             WHERE uid = :uid AND iid = :iid ;
+            ''', uid=userid, iid=inventoryid) 
+
+    @staticmethod
+    def decrease_item(userid, inventoryid):
+        rows = app.db.execute(
+            '''
+            UPDATE CartItems
+            SET quantity = quantity - 1
+            WHERE uid = :uid AND iid = :iid ;
             ''', uid=userid, iid=inventoryid)
+
+    @staticmethod
+    def increase_item(userid, inventoryid):
+        rows = app.db.execute(
+            '''
+            UPDATE CartItems
+            SET quantity = quantity + 1
+            WHERE uid = :uid AND iid = :iid ;
+            ''', uid=userid, iid=inventoryid)
+
+
+class CartWithPrice:
+
+    def __init__(self, uid, iid, quantity, status, unit_price):
+        self.uid = uid
+        self.iid = iid
+        self.quantity = quantity
+        self.status = status
+        self.unit_price = unit_price
+    
+    @staticmethod
+    def get_total_price(items):
+        return sum([item.unit_price * item.quantity for item in items])
