aab441191e05dfaced80f661ca286aff0b105bfc
Yanzheng Wu <yw609@duke.edu>
2024-04-30 18:49:39 -0400

    implemented add cart functionality

--- app/api/sellersProduct.py
+++ app/api/sellersProduct.py
@@ -1,10 +1,12 @@
-from flask import render_template, request
-from flask_login import current_user
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_user, login_required
+
 import datetime
 
 from ..models.product import Product
 from ..models.user import User
 from ..models.sellersProduct import SellersProduct
+from ..models.cart import Cart
 
 from flask import Blueprint
 bp = Blueprint('sellersProduct', __name__)
@@ -16,3 +18,16 @@ def seller_products():
     return render_template('sellersProduct.html',
                            avail_products=products_info,
                            role=User.getRole(current_user.id))
+
+@bp.route('/sellersProduct/increase', methods=['POST'])
+@login_required
+def increase_item():
+    uid = request.form['userid']
+    iid = request.form['inventoryid']
+    quantity = request.form.get('quantity', 1)
+    try:
+        SellersProduct.add_cart(uid, iid, quantity)
+        flash('Product added successfully!')
+    except Exception as e:
+        flash(f'Failed to add product: {str(e)}')
+    return redirect(request.referrer)
